009680b38c112b27e449b801a2c8928cb25c2f0b
Patrick Burns <burnsoft@gmail.com>
2018-06-11 20:51:16 -0500

    fading working

--- src/main.ino
+++ src/main.ino
@@ -72,6 +72,7 @@ charlieLed myLeds[42]  = {
   {E,C}, {H,D}, {G,C}, {H,C}, {G,D}, {A,D}, {E,D}, {B,D},
   {C,D}
 };
+int current = 0;
 
 void setup() {
   randomSeed(analogRead(0));
@@ -80,8 +81,49 @@ void setup() {
 #define NUM_LAUREL 17
 int numOfLEDsToShow = 0;
 unsigned long time;
-void loop() {
 
+void loop() {
+  pingPongAnimation();
+}
+
+void pingPongAnimation(){
+  unsigned long loopCount = 0;                          // used to determine duty cycle of each LED
+  unsigned long timeNow = millis();                     //
+  unsigned long displayTime = 10 + random(90);          // milliseconds to spend at each focus LED in descent
+  while(millis()- timeNow < (displayTime+current*2)) {  // animation slows toward end
+    myCharlie.allClear();
+    loopCount++;
+    // the "snowflake" gets full duty cycle.  When it gets to the end, hold it at the end until the tail collapses
+    myCharlie.ledWrite(myLeds[current], 1);
+    // each member of tail has reduced duty cycle, and never get to the final position
+    if(!(loopCount % 3)) if(current-1 >=0 && current-1 < 16) myCharlie.ledWrite(myLeds[current-1], 1);
+    if(!(loopCount % 6)) if(current-2 >=0 && current-2 < 16) myCharlie.ledWrite(myLeds[current-2], 1);
+    if(!(loopCount % 9)) if(current-3 >=0 && current-3 < 16) myCharlie.ledWrite(myLeds[current-3], 1);
+    if(!(loopCount % 12)) if(current-4 >=0 && current-4 < 16) myCharlie.ledWrite(myLeds[current-4], 1);
+    myCharlie.outRow();
+  }
+
+  current++;
+  if(current>=16) {                          // start over
+    // now fade out the snowflake in that final position #19
+    for(int dutyCycle = 3; dutyCycle <= 15; dutyCycle += 3) {
+      loopCount = 0;
+      timeNow = millis();
+      while(millis() - timeNow < (displayTime+current*2)) { // fade out as slow as animation has achieved by now
+        myCharlie.allClear();
+        loopCount++;
+        if(!(loopCount % dutyCycle)) myCharlie.ledWrite(myLeds[16], 1);
+        else myCharlie.allClear();
+        myCharlie.outRow();
+      }
+    }
+    current = 0;
+    myCharlie.allClear();
+    myCharlie.outRow();            // and then rinse, repeat...after a short pause
+  }
+}
+
+void defaultAnimation(){
   myCharlie.allClear();
   myCharlie.outRow();
 
